314cb836858a107ef27ab2ad6ac205aade64524e
Dmitry <dmitry@192.168.100.4>
2017-02-09 01:45:09 +0300

    NoteAndCalendar

---
+++ Board/Board/CalendarViewController.swift
@@ -0,0 +1,76 @@
+//
+//  CalendarViewController.swift
+//  Board
+//
+//  Created by Dmitry on 08.02.17.
+//  Copyright © 2017 Creator. All rights reserved.
+//
+
+import UIKit
+
+import JTAppleCalendar
+
+extension CalendarViewController: JTAppleCalendarViewDataSource, JTAppleCalendarViewDelegate {
+    
+
+
+    
+    func calendar(_ calendar: JTAppleCalendarView, willDisplayCell cell: JTAppleDayCellView, date: Date, cellState: CellState) {
+        let myCustomCell = cell as! CellView
+        
+        // Setup Cell text
+        myCustomCell.dayLabel.text = cellState.text
+        
+        // Setup text color
+        if cellState.dateBelongsTo == .thisMonth {
+            myCustomCell.dayLabel.textColor = UIColor.black
+        } else {
+            myCustomCell.dayLabel.textColor = UIColor.gray
+        }
+    }
+    
+    
+    func configureCalendar(_ calendar: JTAppleCalendarView) -> ConfigurationParameters {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy MM dd"
+        
+        let startDate = formatter.date(from: "2017 02 01")! // You can use date generated from a formatter
+        let endDate = Date()                                // You can also use dates created from this function
+        let parameters = ConfigurationParameters(startDate: startDate,
+                                                 endDate: endDate,
+                                                 numberOfRows: 6, // Only 1, 2, 3, & 6 are allowed
+            calendar: Calendar.current,
+            generateInDates: .forAllMonths,
+            generateOutDates: .tillEndOfGrid,
+            firstDayOfWeek: .monday)
+        return parameters
+    }
+}
+
+class CalendarViewController: UIViewController {
+
+    
+   
+    @IBOutlet weak var calendarView: JTAppleCalendarView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        calendarView.dataSource = self
+        calendarView.delegate = self
+        calendarView.registerCellViewXib(file: "CellView") // Registering your cell is manditory
+    }
+}
+
+ 
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+
---
+++ Board/Board/CellView.swift
@@ -0,0 +1,16 @@
+//
+//  CellView.swift
+//  Board
+//
+//  Created by Dmitry on 09.02.17.
+//  Copyright © 2017 Creator. All rights reserved.
+//
+
+import UIKit
+
+import JTAppleCalendar
+
+class CellView: JTAppleDayCellView {
+      @IBOutlet var dayLabel: UILabel!
+}
+
